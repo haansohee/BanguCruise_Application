d95b2952531003c68f3e7aadfc5b2344bee28c7c
아기코쟁이 <90755590+haansohee@users.noreply.github.com>
2023-08-25 14:54:40 +0900

    create project

---
+++ BanguCruise/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BanguCruise
+//
+//  Created by 한소희 on 2023/08/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
